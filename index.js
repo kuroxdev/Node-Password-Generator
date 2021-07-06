f3eaca2c41fae44feceff7f5399f46fa69b9a563
jendoubi iheb <iheb@dev.com>
2021-07-06 16:37:13 +0100

    added commander logic

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('test')
+const { program } = require('commander');
+program.version('1.0.1').description('Node password Generator');
+
+
+
+program
+.option('-l,--length <number>','set the length of your password','8')
+.option('-s,--save','Save password in password.txt file')
+.option('-nn,--no-numbers','no numbers will be in your password')
+.option('-ns,--no-symbols','no symbols will be in your password')
+
+
+.parse();
+
+const {length,numbers,symbols,save}= program.opts()
+// generate password fn
+const pwdChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const pwdnumbers = '0123456789';
+const pwdspecials = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
+const generatefn=(length,number,symbols)=>{
+    if(numbers==true && symbols==true){
+
+
+    }
+
+}
